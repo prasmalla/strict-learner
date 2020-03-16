aac106ccb2f5e70ae08fc229d62e18c4032ce13e
prasmalla <malla.prasanna@gmail.com>
2020-03-15 23:39:27 -0400

    nuxt skeleton

---
+++ layouts/default.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer>
+      <span>{{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'start',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'notes',
+          to: '/notes'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'strictlearn.in'
+    }
+  }
+}
+</script>
